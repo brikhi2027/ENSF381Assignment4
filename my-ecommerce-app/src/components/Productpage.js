c5f5bd308361d6e14dbf9d99942b51d28bd08a66
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2024-03-25 19:09:47 -0600

    products are visible now

--- my-ecommerce-app/src/components/Productpage.js
+++ my-ecommerce-app/src/components/Productpage.js
@@ -2,23 +2,23 @@ import React, {useState, useEffect} from 'react';
 import Header from './Header';
 import ProductList from './ProductList';
 import Cart from './Cart';
-import {list} from './Cart';
+//import {list} from './Cart';
 import Footer from './footer';
 
 
 const Productpage = () => {
-    const [cartItems, setCartItems] = useState(list);
+    //const [cartItems, setCartItems] = useState(list);
     
-    useEffect(() => {
-        const currentCart = localStorage.getItem('cartItems');
-        if(currentCart){
-            setCartItems(JSON.parse(currentCart));
-        }
-    }, []); // this will only run on mount
+    //useEffect(() => {
+        //const currentCart = localStorage.getItem('cartItems');
+        //if(currentCart){
+            //setCartItems(JSON.parse(currentCart));
+        //}
+    //}, []); // this will only run on mount
 
-    useEffect(() => {
-        localStorage.setItem('cartItems', JSON.stringify(cartItems))
-    }, [cartItems]); // this will run whenever the array of cart items changes
+    //useEffect(() => {
+        //localStorage.setItem('cartItems', JSON.stringify(cartItems))
+    //}, [cartItems]); // this will run whenever the array of cart items changes
 
     return(
         <div className = "product-page">
